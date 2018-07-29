954099eda658afc366f00066225525e9c3ccea09
Lauren Mwano <ellemwano@users.noreply.github.com>
2018-07-29 15:40:55 +0200

    Create Java Library with 1 joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.mwano.lauren.jokeslibrary.Jokes;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Jokes jokes = new Jokes();
+        String funnyJoke = jokes.getJoke();
+        Toast.makeText(this, funnyJoke, Toast.LENGTH_LONG).show();
     }
 
 
---
+++ jokesLibrary/src/main/java/com/mwano/lauren/jokeslibrary/Jokes.java
@@ -0,0 +1,12 @@
+package com.mwano.lauren.jokeslibrary;
+
+import java.util.ArrayList;
+
+public class Jokes {
+
+    public String getJoke() {
+            return "Why can't you trust stairs?\n" +
+                    "Because they're always up to something.";
+        }
+
+}
