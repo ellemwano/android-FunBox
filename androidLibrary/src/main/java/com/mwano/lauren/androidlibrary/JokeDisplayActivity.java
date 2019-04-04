e76aca4d979c9b7c20cb144a6939eee7b4e6a757
ellemwano <ellemwano@users.noreply.github.com>
2019-04-04 20:58:04 +0200

    Small UI change + Add pic

--- androidLibrary/src/main/java/com/mwano/lauren/androidlibrary/JokeDisplayActivity.java
+++ androidLibrary/src/main/java/com/mwano/lauren/androidlibrary/JokeDisplayActivity.java
@@ -1,10 +1,8 @@
 package com.mwano.lauren.androidlibrary;
 
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class JokeDisplayActivity extends AppCompatActivity{
